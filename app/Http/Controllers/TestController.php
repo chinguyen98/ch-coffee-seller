7b613abf7e174761a4531265dacc6aeb419a630d
ChiNguyen <dacchi6761@gmail.com>
2020-02-12 09:07:04 +0700

    Note test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,7 +10,35 @@ class TestController extends Controller
 {
     public function test()
     {
-        $brand = Coffee::find(10)->brand;
-        return $brand->name;
+        // $brand = Coffee::find(10)->brand;
+        // return $brand->name;
+
+        // $brands = Brand::all();
+        // foreach ($brands as $brand) {
+        //     echo $brand->name . ",";
+        // }
+
+        // $coffees = Coffee::all();
+        // $coffees = $coffees->reject(function ($coffee) {
+        //     return $coffee->price < 1000000;
+        // });
+        // foreach ($coffees as $coffee) {
+        //     echo $coffee->price . ",";
+        // }
+
+        // Coffee::chunk(5, function ($coffees) {
+        //     foreach ($coffees as $coffee) {
+        //         echo $coffee->id . ',';
+        //     }
+        //     echo "End!";
+        // });
+
+        // $coffeesWithSubquery = Coffee::addSelect(['brand_name' => Brand::select('name')->whereColumn('id', 'id_brand')])->get();
+        // foreach ($coffeesWithSubquery as $item) {
+        //     echo $item->brand_name;
+        // }
+
+        // $coffee = Brand::find(1)->coffees()->where('id', 4)->first();
+        // echo $coffee->name;
     }
 }
